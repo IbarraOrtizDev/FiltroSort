e1f6363829da22ecfa12353047271e93b52182d7
IbarraOrtizDev <ortizalexander2244@gmail.com>
2024-04-02 09:02:49 -0500

    Se organiza modelo de filtros para evitar parametros innecesarios

--- FiltroSort/Models/FilterSoftModel.cs
+++ FiltroSort/Models/FilterSoftModel.cs
@@ -10,24 +10,21 @@ public class FilterSoftModel
     public string Filter { get; set; } = string.Empty;
 
     /// <summary>
-    ///  Establece la propiedad por la que se debe ordenar   
+    ///  Establece la propiedad por la que se debe ordenar  
+    ///  Se debe establecer de la siguiente forma: desc:nombrePropiedad o asc:nombrePropiedad
     /// </summary>
     public string Sort { get; set; } = string.Empty;
 
-    /// <summary>
-    /// Establece si el ordenamiento debe ser ascendente o descendente
-    /// </summary>
-    public OrderEnum Order { get; set; } = OrderEnum.Asc;
 
     /// <summary>
     /// Establece el número de página
     /// </summary>
-    public int? Page { get; set; } = 1;
+    public int? Page { get; set; }
 
     /// <summary>
     /// Establece el número de elementos por página
     /// </summary>
-    public int? PageSize { get; set; } = 10;
+    public int? PageSize { get; set; }
 
 
 }
--- FiltroSortApi/Controllers/ValuesController.cs
+++ FiltroSortApi/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using FiltroSortApi.DTOs;
 using FiltroSortApi.Services;
 using FilterSort.Models;
 using FiltroSort;
+using System.Linq.Expressions;
 namespace FiltroSortApi.Controllers
 {
     [Route("api/[controller]")]
@@ -23,7 +24,46 @@ namespace FiltroSortApi.Controllers
             //var data = _serviceData.GetData().AsQueryable().Where(filt).ToList();
             var page = dataQuery.Page??1;
             var pageSize = dataQuery.PageSize??10;
-            var data = _serviceData.GetData().AsQueryable().Where(filt).Skip((page-1)*pageSize).Take(pageSize);
+            IQueryable<DataDTO> query = _serviceData.GetData().AsQueryable();
+            if(filt != null)
+            {
+                query = query.Where(filt);
+            }
+
+            if(!string.IsNullOrEmpty(dataQuery.Sort))
+            {
+                var type = dataQuery.Sort.Split(":")[0];
+                if (type != "asc" && type != "desc")
+                {
+                    return BadRequest("El tipo de ordenamiento no es válido");
+                }
+
+                var propertyName = dataQuery.Sort.Split(":")[1];
+                var property = typeof(DataDTO).GetProperty(propertyName);
+
+                if (property == null)
+                {
+                    return BadRequest("La propiedad de ordenamiento no existe");
+                }
+
+                Expression<Func<DataDTO, object>> expression = x => property.GetValue(x);
+
+                if (type == "asc")
+                {
+                    query = query.OrderBy(expression);
+
+                }
+                else
+                {
+                    query = query.OrderByDescending(expression);
+                }
+            }
+
+            if(dataQuery.Page.HasValue && dataQuery.PageSize.HasValue)
+            {
+                query = query.Skip((page-1)*pageSize).Take(pageSize);
+            }
+            var data = query.ToList();
             return Ok(data);
         }
     }
